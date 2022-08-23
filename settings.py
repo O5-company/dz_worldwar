a4120297f36ba0a60ae08a98c681436cf08599e5
morty-idhyuajjcbdchjdehsr <109400820+morty-idhyuajjcbdchjdehsr@users.noreply.github.com>
2022-08-23 16:49:08 +0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,25 @@
+bg_color1=(200,255,255)
+bg_color2=(60,60,60)
+bg_color3=(240,50,92)
+
+ship_speed=5
+ship_acc=1
+stop_acc =0.1
+max_bullet_num=30
+bullet_speed=1
+
+bullet_fre=15
+alien_bullet_fre =200
+max_alien_bullet_num=30
+
+least_bullet_fre = 7
+
+alien_number=10
+alien1_number =2
+alien_speed=1
+
+max_life=3
+
+#游戏节奏
+speedup_scale=1.05
+speed_list=(ship_speed,bullet_speed,alien_speed,alien_number, bullet_fre,max_bullet_num)
